64763f20cf2664cf1331ee9ad6ef53d361f9634d
Carl & Emily Osowski & Jillian Campbell <pair+carlincharge.emosowski.jillwc07@devbootcamp.com>
2016-04-15 19:06:47 -0400

    adds all models and migrations partial complete

---
+++ app/controllers/decks_controller.rb
@@ -0,0 +1,12 @@
+get '/decks' do
+  @decks = Deck.all
+
+  erb :'/decks/index'
+end
+
+get '/decks/:id' do
+
+@deck = Deck.find_by(id: params[:id])
+
+erb :'/decks/show'
+end
---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  # has_many :games
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true
+
+  has_secure_password
+
+end
