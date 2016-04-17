b535499a979a4a39479ee8ad2996da3a069692ce
Carl & Emily Osowski & Jillian Campbell <pair+carlincharge.emosowski.jillwc07@devbootcamp.com>
2016-04-16 22:42:01 -0400

    Add change back to allowing to play all cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,67 @@
-DECK_COUNT = 3
-CARD_COUNT = 5
+# DECK_COUNT = 3
+# CARD_COUNT = 5
 
-DECK_COUNT.times do
-  Deck.create(
-    name: Faker::Hipster.sentence,
-    description: Faker::Hipster.paragraph
-    )
-end
+# DECK_COUNT.times do
+#   Deck.create(
+#     name: Faker::Hipster.sentence,
+#     description: Faker::Hipster.paragraph
+#     )
+# end
 
-n = 1
+# n = 1
 
-CARD_COUNT.times do
-  Card.create(
-    question: Faker::Hacker.say_something_smart,
-    answer: "a",
-    deck_id: n
+# CARD_COUNT.times do
+#   Card.create(
+#     question: Faker::Hacker.say_something_smart,
+#     answer: "a",
+#     deck_id: n
+#   )
+# end
+
+# CARD_COUNT.times do
+#   Card.create(
+#     question: Faker::Hacker.say_something_smart,
+#     answer: "a",
+#     deck_id: n + 1
+#   )
+# end
+
+# CARD_COUNT.times do
+#   Card.create(
+#     question: Faker::Hacker.say_something_smart,
+#     answer: "a",
+#     deck_id: n + 2
+#   )
+# end
+
+Deck.create!(
+  name: "Music",
+  description:"Test your music knowledge"
   )
-end
 
-CARD_COUNT.times do
-  Card.create(
-    question: Faker::Hacker.say_something_smart,
-    answer: "a",
-    deck_id: n + 1
-  )
-end
 
-CARD_COUNT.times do
-  Card.create(
-    question: Faker::Hacker.say_something_smart,
-    answer: "a",
-    deck_id: n + 2
+
+Card.create!(
+  question: "What is Michael Jackson's first name?",
+  answer: "Michael",
+  deck_id: 1
   )
-end
+Card.create!(
+  question: "What year was Taylor Swift born?",
+  answer: "1989",
+  deck_id: 1
+  )
+
+Card.create!(
+  question: "How many number one albums did The Beatles have?",
+  answer: "17",
+  deck_id: 1
+  )
+
+Card.create!(
+  question: "What instrument does guitarist Slash play?",
+  answer: "guitar",
+  deck_id: 1
+  )
 
 
